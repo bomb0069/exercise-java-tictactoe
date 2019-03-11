37221343524372feb898264c8374bbece41bd9c0
karan sivarat <bomb0069@gmail.com>
2019-03-11 15:33:49 +0700

    IMPLEMENT : X or O can be win at first rows

--- src/main/java/TicTacToe.java
+++ src/main/java/TicTacToe.java
@@ -1,16 +1,33 @@
 public class TicTacToe {
     public static final int NOT_FINISHED = 0;
     public static final int WIN_BY_O = 1;
+    public static final int WIN_BY_X = 2;
+    private static final int O_PLAYER = 1;
+    private static final int X_PLAYER = 2;
 
     int[][] board = new int[3][3];
 
+    boolean turn_of_O = true;
+
     public int getStatus() {
+
+        if (board[0][0] == 2 && board[0][1] == 2 && board[0][2] == 2)
+            return 2;
+
         if (board[0][0] == 1 && board[0][1] == 1 && board[0][2] == 1)
             return 1;
+
         return 0;
     }
 
     public void play(int coordinateX, int coordinateY) {
-        board[coordinateX][coordinateY] = 1;
+        int player = 0;
+        if (turn_of_O)
+            player = O_PLAYER;
+        else
+            player = X_PLAYER;
+
+        board[coordinateX][coordinateY] = player;
+        turn_of_O = !turn_of_O;
     }
 }
--- src/test/java/TicTacToeTest.java
+++ src/test/java/TicTacToeTest.java
@@ -30,4 +30,17 @@ public class TicTacToeTest {
         Assert.assertEquals(game.WIN_BY_O, actualStatus);
     }
 
+    @Test
+    public void all_of_first_row_with_X_should_status_win_by_X () {
+        TicTacToe game = new TicTacToe();
+        game.play(1,0);
+        game.play(0,0);
+        game.play(1,1);
+        game.play(0,1);
+        game.play(2,0);
+        game.play(0,2);
+        int actualStatus = game.getStatus();
+        Assert.assertEquals(game.WIN_BY_X, actualStatus);
+    }
+
 }
